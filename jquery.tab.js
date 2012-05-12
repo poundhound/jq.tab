6bf25b165ae623afb65e2cfbab97601c325aaeaa
poundhound <k.norizo@gmail.com>
2012-05-13 00:19:09 +0900

    Ver1.1
    jQuery Cookieプラグインを利用して、現在開いているタブを保持

--- jquery.tab.js
+++ jquery.tab.js
@@ -19,15 +19,24 @@
 		}, options);
 	
 		$(o.tabElement, o.targetID).each(function() {
-			$(o.boxElement, o.targetID).not(':first').hide();
-			$(o.tabElement, o.targetID).eq(0).addClass('selected');
 			
-			$(this).click(function() {
+			var selectedTab = $.cookie(o.targetID + 'selectedTab');
+			if (selectedTab) {
+				$(o.boxElement, o.targetID).not(':eq(' +selectedTab+')').hide();
+				$(o.tabElement, o.targetID).eq(selectedTab).addClass('selected');				
+			} else {
+				$(o.boxElement, o.targetID).not(':first').hide();
+				$(o.tabElement, o.targetID).eq(0).addClass('selected');
+			}
+			
+			$(this).click(function(){
 				var tabIndex = $(o.tabElement, o.targetID).index(this);
+				$.cookie(o.targetID + 'selectedTab', tabIndex);
 				$(o.tabElement, o.targetID).removeClass('selected');
 				$(this).addClass('selected');
 				$(o.boxElement, o.targetID).hide().eq(tabIndex).addClass('selected').fadeIn();
 			});
+			
 		});
 		return this;
 	};
